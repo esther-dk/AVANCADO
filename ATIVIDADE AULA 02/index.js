06d81e8569f96603ac79320eb13e8c5dbffc2620
2022esther <106917457+2022esther@users.noreply.github.com>
2024-09-27 11:03:27 -0300

    Add files via upload

---
+++ ATIVIDADE AULA 02/index.js
@@ -0,0 +1,38 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    console.log(req.url);
+
+    if (req.method === 'GET' && req.url === '/') {
+        fs.readFile(
+            './index.html',
+            { encoding: 'utf-8', flag: 'r' },
+            function (err, d) {
+                if (!err) {
+                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                    res.write(d);
+                    res.end();
+                }
+            }
+        );
+    } else {
+        fs.readFile(    
+            './404.html',
+            { encoding: 'utf-8', flag: 'r' },
+            function (err, d) {
+                if (!err) {
+                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                    res.write(d);
+                    res.end();
+                }
+            }
+        );
+    }
+});
+
+const portaDoservidor = 8080;
+
+server.listen(porta, () => {
+    console.log(`Porta: ${portaDoservidor}!`);
+});
